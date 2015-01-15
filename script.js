eacacc7fbb6f60e4c499edf3528bc7d07613fdf6
Sofie <andersson.sofie@hotmail.se>
2015-01-15 19:42:30 +0100

    Fixed mobile menu btn

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 $(document).on("ready", init);
 
 function init() {
+    
     $('.sub-menu').hide();
-    $('.menu > li, ul.sub-menu > li').on("mouseenter", function () {
-        $('> ul.sub-menu',this).slideToggle('slow');
-    });
+        $('.menu > li, ul.sub-menu > li').on("mouseenter", function () {
+            $('> ul.sub-menu',this).slideToggle('slow');
+        });
 
-    $('.menu > li, ul.sub-menu > li').on("mouseleave", function () {
-        $('> ul.sub-menu',this).slideToggle('slow');
-    });
+        $('.menu > li, ul.sub-menu > li').on("mouseleave", function () {
+            $('> ul.sub-menu',this).slideToggle('slow');
+        });
+
 
     hideShowMenu();
     $(window).on("resize", hideShowMenu);
@@ -19,15 +21,18 @@ function init() {
                 $('.menu').hide();
             }
             $("header img").attr("id", "mobile");
+            
         } else {
             $("header img").removeAttr("id");
             $(".menu").show();
         }
     };
-
+    
     $("header img").on("click", function() {
         if($("header img").attr("id") == "mobile") {
             $('.menu').slideToggle('slow');
         }
     });
+
+    
 }
