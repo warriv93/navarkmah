dbe0d612e9eb2255a0425f1ba004cb7795488774
Simon Gullstrand <warriv93@gmail.com>
2015-01-15 11:51:25 +0100

    script för menyer
    fått dem att hoppa ner och upp

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 $(document).on("ready", init);
 
 function init() {
-
+    $('.sub-menu').hide();
+        $('.menu li, .sub-menu > li').hover(function () {
+        if ($('> ul.sub-menu',this).length > 0) {
+            $('> ul.sub-menu',this).stop().slideDown('slow');
+        }
+        },function () {
+        if ($('> ul.sub-menu',this).length > 0) {
+            $('> ul.sub-menu',this).stop().slideUp('slow');
+        }
+    });
 }
